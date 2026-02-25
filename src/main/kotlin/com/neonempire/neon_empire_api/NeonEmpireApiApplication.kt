e40ea80d3d10aff55960c4aee111bb71a9e3cb15
Hassan Ali <alisy@tcd.ie>
2026-02-25 00:51:03 +0000

    first commit

---
+++ src/main/kotlin/com/neonempire/neon_empire_api/NeonEmpireApiApplication.kt
@@ -0,0 +1,11 @@
+package com.neonempire.neon_empire_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NeonEmpireApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NeonEmpireApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/neonempire/neon_empire_api/NeonEmpireApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.neonempire.neon_empire_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NeonEmpireApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
